ccbb0eafe5082a69c7a878db0059c496d7a26a7f
gaopengfei <gaopengfei@soyoung.com>
2019-05-29 10:32:08 +0800

    补充测试用例

--- logs/log_test.go
+++ logs/log_test.go
@@ -1,16 +1,31 @@
 package logs
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestLog(t *testing.T) {
-	callAll(LevelDebug, "测试消息")
+	levelPool := []int{
+		LevelEmergency,
+		LevelAlert,
+		LevelCritical,
+		LevelError,
+		LevelWarning,
+		LevelNotice,
+		LevelInformational,
+		LevelDebug,
+	}
+	for _, level := range levelPool {
+		callAll(level, "测试消息")
+	}
+
 }
 
 func callAll(level int, msg string) {
 	logger := NewLogger()
 	logger.SetLevel(level)
+	fmt.Printf(">>>>>>>>>>>>>>>>>>>>>>>>level: %d \n", level)
 
 	logger.Emergency(msg)
 	logger.Alert(msg)
