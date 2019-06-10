3aa79e34a8f02fe9fa7f8d4a535e6e915582b72a
gaopengfei <gaopengfei@soyoung.com>
2019-06-10 20:37:49 +0800

    加测试用例

--- logs/file_test.go
+++ logs/file_test.go
@@ -27,19 +27,34 @@ func TestFile(t *testing.T) {
 
 }
 
-// func TestFileLog(t *testing.T) {
-// 	t.Log("测试fileLog")
+func TestFileMuti(t *testing.T) {
+	t.Log("多 logger 测试")
+	config := `{
+		"filename": "logs/test.log"
+	}`
 
-// 	config := `{
-// 		"filename": "logs/test.log",
-// 		"level" : 7
-// 	}`
+	log1 := NewLogger()
+	log1.SetLogger(AdapterFile, config)
+	log1.Info("test1")
 
-// 	logger := NewLogger()
-// 	err := logger.SetLogger(AdapterFile, config)
-// 	if err != nil {
-// 		t.Errorf("写入错误, err: %v \n", err)
-// 	}
-// 	logger.Info("测试文件消息")
-// 	logger.Close()
-// }
+	log2 := NewLogger()
+	log2.SetLogger(AdapterFile, config)
+	log2.Info("test2")
+}
+
+func TestFileLog(t *testing.T) {
+	t.Log("测试 fileLog")
+
+	config := `{
+		"filename": "logs/test.log",
+		"level" : 7
+	}`
+
+	logger := NewLogger()
+	err := logger.SetLogger(AdapterFile, config)
+	if err != nil {
+		t.Errorf("写入错误, err: %v \n", err)
+	}
+	logger.Info("测试文件消息")
+	logger.Close()
+}
