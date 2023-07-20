439ce49cd4de601a771e02e0dfbb707f60856c82
gaopengfei <gaopengfei@youshu.cc>
2023-07-20 20:20:21 +0800

    补充函数

--- convert/convert.go
+++ convert/convert.go
@@ -1,6 +1,8 @@
 package convert
 
 import (
+	"fmt"
+	"reflect"
 	"strconv"
 	"strings"
 )
@@ -117,4 +119,29 @@ func Div(Num int) string {
 		Str = Slice[value] + Str //因为数据切分后存储顺序是反的，所以Str要放在后面
 	}
 	return Str
+}
+
+// ToMap 结构体转为Map[string]interface{}
+func ToMap(in interface{}, tagName string) (map[string]interface{}, error) {
+	out := make(map[string]interface{})
+
+	v := reflect.ValueOf(in)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+
+	if v.Kind() != reflect.Struct { // 非结构体返回错误提示
+		return nil, fmt.Errorf("ToMap only accepts struct or struct pointer; got %T", v)
+	}
+
+	t := v.Type()
+	// 遍历结构体字段
+	// 指定tagName值为map中key;字段值为map中value
+	for i := 0; i < v.NumField(); i++ {
+		fi := t.Field(i)
+		if tagValue := fi.Tag.Get(tagName); tagValue != "" {
+			out[tagValue] = v.Field(i).Interface()
+		}
+	}
+	return out, nil
 }
